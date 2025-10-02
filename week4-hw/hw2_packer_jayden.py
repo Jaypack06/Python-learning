795c96b9163c3f52e51840f39c141fb8199caed6
Jayden Packer <jaydenpacker267@gmail.com>
2025-10-02 10:02:30 -0400

    Started homework

--- week4-hw/hw2_packer_jayden.py
+++ week4-hw/hw2_packer_jayden.py
@@ -18,3 +18,22 @@ def problem1():
     random_arr = np.random.rand(2,5)
     print(random_arr)
 problem1()
+
+def problem2():
+    arr_a = np.array([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]])
+    arr_b = np.array([10, 20, 30])
+    
+    result_add = arr_a + arr_b
+    
+    result_multiply = arr_a * arr_b
+    
+    result_square = arr_a ** 2
+    
+    column_means = np.mean(arr_a, axis=0)
+    
+    centered_arr = arr_a - column_means
+    
+    return result_add, result_multiply, result_square, column_means, centered_arr
+print(problem2())
